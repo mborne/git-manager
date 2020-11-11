647dbd23417fefc3b20ee527715cda7c70f8fa18
MBorne <mickael.borne@ign.fr>
2020-11-11 17:14:58 +0100

    avoid shebang display while running phar

--- Makefile
+++ Makefile
@@ -3,7 +3,9 @@ all: dist
 
 .PHONY: dist
 dist: vendor
+	rm -f dist/git-manager.phar
 	php -d phar.readonly=0 build-phar.php
+	chmod +x dist/git-manager.phar
 
 test: vendor
 	vendor/bin/phpunit -c phpunit.xml
--- build-phar.php
+++ build-phar.php
@@ -3,8 +3,15 @@ $buildRoot = __DIR__;
 
 $phar = new Phar($buildRoot . '/dist/git-manager.phar', 0, 'git-manager.phar');
 
+$phar->startBuffering();
+
 $include = '/^(?=(.*src|.*app|.*public|.*templates|.*bin|.*vendor))(.*)$/i';
-
 $phar->buildFromDirectory($buildRoot, $include);
-$phar->setStub("#!/usr/bin/env php\n" . $phar->createDefaultStub("bin/console"));
 
+/* avoid first line display */
+$content = file_get_contents(__DIR__.'/bin/console');
+$content = preg_replace('{^#!/usr/bin/env php\s*}', '', $content);
+$phar->addFromString('bin/console',$content);
+
+$phar->setStub("#!/usr/bin/env php\n".$phar->createDefaultStub("bin/console"));
+$phar->stopBuffering();
