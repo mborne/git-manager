debfccf052556c979fe6c18b8f7b8db5a7e5d49f
mborne <mborne@users.noreply.github.com>
2022-09-24 10:43:09 +0200

    ci - add gitlab check...

--- .ci/check-gitlab.sh
+++ .ci/check-gitlab.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_DIR=$(dirname $SCRIPT_DIR)
 
-php ${SCRIPT_DIR}/dist/git-manager.phar git:fetch-all --users=mborne https://gitlab.com $GITLAB_TOKEN
+php ${PROJECT_DIR}/dist/git-manager.phar git:fetch-all --users=mborne https://gitlab.com $GITLAB_TOKEN
